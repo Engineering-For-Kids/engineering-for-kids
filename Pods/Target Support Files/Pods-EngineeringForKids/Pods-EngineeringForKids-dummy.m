9eac5766322c090de5b55ecd9ab7b2f06c1de66d
Lillie Zhou <zlillie@lawn-128-61-78-104.lawn.gatech.edu>
2018-02-23 21:27:38 -0500

    Add Firebase configuration

---
+++ Pods/Target Support Files/Pods-EngineeringForKids/Pods-EngineeringForKids-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EngineeringForKids : NSObject
+@end
+@implementation PodsDummy_Pods_EngineeringForKids
+@end
---
+++ Pods/Target Support Files/Pods-EngineeringForKidsTests/Pods-EngineeringForKidsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EngineeringForKidsTests : NSObject
+@end
+@implementation PodsDummy_Pods_EngineeringForKidsTests
+@end
